ce7049ae6b4379de043c1f0f5533d280dc914ad5
Roney Pires <roneypiresf@outlook.com>
2024-10-23 18:29:06 -0300

    aula if v2

---
+++ aula_5.py
@@ -0,0 +1,7 @@
+entrada= input('voce quer entrar ou sair?')
+if entrada == 'entrar':
+    print('voce entrou no sistema')
+elif entrada == 'sair':
+    print('voce saiu no sistema')
+else:
+    print('voce não digitou entrar ou sair')
