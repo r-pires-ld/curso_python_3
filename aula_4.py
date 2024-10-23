7b4d83ff6c511ec8cfacfe8c7dfff3609f2b880b
Roney Pires <roneypiresf@outlook.com>
2024-10-23 18:22:04 -0300

    aula input v2

--- aula_4.py
+++ aula_4.py
@@ -2,6 +2,16 @@
 #nome = input("Qual seu nome?")
 #print(f"seu nome é {nome}")
 
-numero_1 = input("Digite um numero qualquer: ")
-numero_2 = input("Digite outro numero qualquer: ")
-print(f'A soma dos numeros é: {numero_1+numero_2}')
+#sempre fazer a conversão da variavel para receber o valor
+
+''''
+modo errado de se fazer
+numero_1 = int(input("Digite um numero qualquer: "))
+numero_2 = int(input("Digite outro numero qualquer: "))
+'''
+
+numero_1 = input('Digite um numero: ')
+conv_numero_1 = int(numero_1)
+numero_2=input('Digite outro numero')
+conv_numero_2 = int(numero_2)
+print(f'A soma dos numeros é: {conv_numero_1+conv_numero_2}')
