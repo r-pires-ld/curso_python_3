bc4f6e17b81f428950b3d3eaba7e6bc736bdff9e
Roney Pires <roneypiresf@outlook.com>
2024-10-23 18:15:14 -0300

    aula input

---
+++ aula_4.py
@@ -0,0 +1,7 @@
+#input sempre retorna uma string
+#nome = input("Qual seu nome?")
+#print(f"seu nome é {nome}")
+
+numero_1 = input("Digite um numero qualquer: ")
+numero_2 = input("Digite outro numero qualquer: ")
+print(f'A soma dos numeros é: {numero_1+numero_2}')
