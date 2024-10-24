dc1ada49b58e4ccc48e77e9aef8c856a3b9a1023
Roney Pires <roneypiresf@outlook.com>
2024-10-24 11:06:41 -0300

    aula_6 v1

--- aula_5.py
+++ aula_5.py
@@ -3,5 +3,13 @@ if entrada == 'entrar':
     print('voce entrou no sistema')
 elif entrada == 'sair':
     print('voce saiu no sistema')
+elif entrada =='admin':
+    senha = input('Digite a senha: ')
+    if senha =='senha':
+        print('Voce entrou no modo administrador')
+    else:
+        print('senha incorreta')
 else:
     print('voce não digitou entrar ou sair')
+
+print('sistema logout')
